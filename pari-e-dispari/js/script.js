83bcb32e03037051fde8da8f4d9f1ca5111db77d
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-21 01:15:25 +0200

    scelta-pari-o-dispari-metodo-sicurezza-true-false

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -8,12 +8,31 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 Bonus: L’inserimento avviene tramite un campo input
 Consigli del giorno
-
+1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 */
 
-// PROCEDIMENTO:
-// 
-// 
-// 
-// 
-// 
+
+
+let evenOdd;
+
+// Variabile con metodo boolean per la scelta del pari e dispari
+let isEvenOdd = true;
+
+
+while(isEvenOdd){
+  
+  evenOdd = prompt('Cosa scegli? Pari, o dispari?');
+  
+  // finché la stringa "evenOdd" non risulta che sia o pari o dispari, richiedo il prompt
+  if(evenOdd !== 'pari' && evenOdd !== 'dispari'){
+    alert('Non hai scritto corretamente la tua scelta. Riprova');
+  }
+  
+  else{
+    isEvenOdd = false;
+  }
+}
+
+console.log('Hai scelto: ', evenOdd)
+
